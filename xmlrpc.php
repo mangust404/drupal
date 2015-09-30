20748490965943289c70581d994a4500fbafb3f1
Riki_tiki_tavi <mangust404@gmail.com>
2015-09-30 23:10:17 +0600

    xmlrpc tests, syntax fixes

--- xmlrpc.php
+++ xmlrpc.php
@@ -4,6 +4,7 @@
  * @file
  * PHP page for handling incoming XML-RPC requests from clients.
  */
+define('DRUPAL_ROOT', getcwd());
 
 include_once './includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
